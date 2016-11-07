6450f0ab9d867a0b85a1c0b1015aba1266eed904
Kadyn Neale <kneale95@hotmail.ca>
2016-11-06 20:06:08 -0800

    header 80% footer 80%

--- themes/inhabitent/redstarter-master/footer.php
+++ themes/inhabitent/redstarter-master/footer.php
@@ -11,7 +11,22 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+					<div class="footer containter">
+						<!--<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>-->
+						<div class = "contact-info footer-info">
+							<h3>CONTACT INFO</h3>
+							<p><a href="#">info@inhabitent.com</a></p>
+							<p><a href="#">778-456-7891</a></p>
+						</div>
+						<div class="hours footer-info">
+							<h3>BUSINESS HOURS</h3>
+							<p><span>Monday-Friday:</span> 9am-5pm</p>
+							<p><span>Saturday:</span> 10am-2pm</p>
+							<p><span>Sunday:</span> Closed</p>
+						</div>
+						<a href="#" class= "footer-image footer-info"></a>
+					</div>
+					<p class="copyright-footer">COPYRIGHT 2016 INHABITENT</p>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
