f9131bf1bef82d50c0a41812125d59a336d171cb
Kadyn Neale <kneale95@hotmail.ca>
2016-11-14 08:50:55 -0800

    shop 70%

--- themes/inhabitent/redstarter-master/functions.php
+++ themes/inhabitent/redstarter-master/functions.php
@@ -103,7 +103,3 @@ require get_template_directory() . '/inc/template-tags.php';
  */
 require get_template_directory() . '/inc/extras.php';
 
-function enqueue_our_required_stylesheets(){
-	wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'); 
-}
-add_action('wp_enqueue_scripts','enqueue_our_required_stylesheets');
--- themes/inhabitent/redstarter-master/inc/extras.php
+++ themes/inhabitent/redstarter-master/inc/extras.php
@@ -108,3 +108,8 @@ function red_wp_trim_excerpt( $text ) {
 
 remove_filter( 'get_the_excerpt', 'wp_trim_excerpt' );
 add_filter( 'get_the_excerpt', 'red_wp_trim_excerpt' );
+
+function enqueue_our_required_stylesheets(){
+	wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'); 
+}
+add_action('wp_enqueue_scripts','enqueue_our_required_stylesheets');
